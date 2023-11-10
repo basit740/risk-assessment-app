9da0f3194698c9ed748f7f8fe430bb6a946059aa
Basit Ali <alib4111@gmail.com>
2023-11-10 06:17:04 +0500

    design fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import RiskAssessment from './components/RiskAssessment';
 function App() {
 	return (
 		<div className='container mx-auto p-4'>
-			<h1 className='text-center font-extrabold bg-slate-600 py-6 px-3 uppercase text-white text-3xl'>
+			<h1 className='text-center font-extrabold bg-slate-600 py-6 px-3 uppercase text-white text-3xl flex align-middle justify-between'>
+				<img
+					width='50px'
+					height='50px'
+					src='https://gowithcanvas.com/wp-content/uploads/2023/01/Canvas_Icon_Color_.png'
+					alt=''
+				/>{' '}
 				Risk Score Calculation
 			</h1>
 			<RiskAssessment />
--- src/components/RiskAssessment.jsx
+++ src/components/RiskAssessment.jsx
@@ -121,6 +121,7 @@ const RiskAssessment = () => {
 				</div>
 				<button
 					type='submit'
+					id='submit'
 					className='inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
 				>
 					Submit
